236d29f9af805506903370ec6ca9b6ac52aefeb6
Hdaccoder <paul.junior418@yahoo.com>
2025-10-13 02:18:59 +0100

    chore: stop tracking build output

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.tsx -> @/components/MapView\":{\"id\":\"app\\\\page.tsx -> @/components/MapView\",\"files\":[\"static/css/_app-pages-browser_src_components_MapView_tsx.css\",\"static/chunks/_app-pages-browser_src_components_MapView_tsx.js\"]},\"lib\\\\auth.ts -> firebase/auth\":{\"id\":\"lib\\\\auth.ts -> firebase/auth\",\"files\":[]}}"
